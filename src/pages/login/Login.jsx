f6df9d0709e8660b99de16d3bcbce88d15df429a
GabrieelAraujoo <gbrielaraujo796@gmail.com>
2023-08-08 16:47:19 -0300

    criação da tela login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Main } from "../../layout/Main";
 import { Container } from "../../layout/Container";
-import { Flex } from "@chakra-ui/react";
+import { Flex, Text, Input, Button ,Stack, InputGroup,InputRightElement,Checkbox} from "@chakra-ui/react";
+import { ViewIcon} from '@chakra-ui/icons'
 
 function Login() {
+
   return (
     <Main>
       <Container>
@@ -13,8 +15,26 @@ function Login() {
           justifyContent="center"
           alignItems="center"
         >
-          <Flex backgroundColor="#c5c5c5" width="40%" height="350px">
-            Menu
+          <Flex backgroundColor="#fff" width="40%" height="350px" direction="column"  padding= "5rem"  alignItems="center">
+            <Text color="#558085" > Seja Bem-Vindo (a) </Text>
+            <Input  type='text' placeholder='E-mail' width="428px" height="50px" />
+            { <Stack spacing={4}>
+               <InputGroup>
+                  <Input type='password' placeholder='Senha'
+                  width="428px" height="50px" />
+            <InputRightElement>
+                  <ViewIcon color='#558085' />
+            </InputRightElement>
+              </InputGroup>
+              </Stack> }
+              <Flex alignItems="basline" >
+                  <Checkbox colorScheme="red" defaultChecked align="left" >Lembre-se de mim</Checkbox>
+                  <Button color="#558085" variant='link'>Esqueceu a senha ? </Button>
+              </Flex>
+                  <Button colorScheme='teal' size='lg' width="428px" height="35px">Button</Button>
+                  <Button color="#558085" variant='link'> Ainda não possui uma conta ? </Button>
+                    
+                    
           </Flex>
         </Flex>
       </Container>
