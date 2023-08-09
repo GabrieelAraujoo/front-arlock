361439ad97a0e71d7dcc544984b82466f951211a
GabrieelAraujoo <gbrielaraujo796@gmail.com>
2023-08-09 16:43:40 -0300

    tela de login atualizada

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Main } from "../../layout/Main";
 import { Container } from "../../layout/Container";
-import { Flex, Text, Input, Button ,Stack, InputGroup,InputRightElement,Checkbox} from "@chakra-ui/react";
+import { Flex, Text, Input, Button ,Stack, InputGroup,InputRightElement,Checkbox,Divider} from "@chakra-ui/react";
 import { ViewIcon} from '@chakra-ui/icons'
 
 function Login() {
@@ -14,24 +14,30 @@ function Login() {
           height="91%"
           justifyContent="center"
           alignItems="center"
+          boxShadow=" 15px 15px 25px rgb(0, 0, 0, .5);"
+
         >
-          <Flex backgroundColor="#fff" width="40%" height="350px" direction="column"  padding= "5rem"  alignItems="center">
-            <Text color="#558085" > Seja Bem-Vindo (a) </Text>
-            <Input  type='text' placeholder='E-mail' width="428px" height="50px" />
-            { <Stack spacing={4}>
+          <Flex boxShadow=" 15px 25px 35px rgb(0, 0, 0, .5);" backgroundColor="#fff" width="508px" height="478px" direction="column"  padding= "2rem"  alignItems="center" marginBottom="30px" borderRadius="10px">
+            <Text margin= "0 0 50px" color="#558085" fontSize="30px"> Seja bem-vindo (a) </Text>
+            <Flex direction="column" marginBottom="10px" >
+            <Input type='text'boxShadow=" 0 10px 15px rgb(0, 0, 0, .5);" placeholder='E-mail' width="428px" height="60px" border="1px" borderColor="#EDE7F6"  marginBottom="20px"/>
+            { <Stack >
                <InputGroup>
-                  <Input type='password' placeholder='Senha'
-                  width="428px" height="50px" />
+                  <Input type='password' boxShadow=" 0 10px 15px rgb(0, 0, 0, .5);" placeholder='Senha' width="428px" height="60px"  border="1px" borderColor="#EDE7F6" />
             <InputRightElement>
-                  <ViewIcon color='#558085' />
+                  <ViewIcon color='black' />
             </InputRightElement>
               </InputGroup>
               </Stack> }
-              <Flex alignItems="basline" >
-                  <Checkbox colorScheme="red" defaultChecked align="left" >Lembre-se de mim</Checkbox>
-                  <Button color="#558085" variant='link'>Esqueceu a senha ? </Button>
               </Flex>
-                  <Button colorScheme='teal' size='lg' width="428px" height="35px">Button</Button>
+              <Flex width="full" justifyContent="space-between">
+                  <Checkbox Spacer="2" color="blueblack" defaultChecked align="left" >Lembre-se de mim</Checkbox>
+                  
+                  <Button  color="#558085" variant='link'>Esqueceu a senha ? </Button>
+              </Flex>
+                     
+                  <Button colorScheme='teal' boxShadow=" 0 15px 25px rgb(0, 0, 0, .5);" size='lg' width="428px" height="60px" margin="20px 0 40px">Entrar</Button>
+                  <Divider  />
                   <Button color="#558085" variant='link'> Ainda não possui uma conta ? </Button>
                     
                     
