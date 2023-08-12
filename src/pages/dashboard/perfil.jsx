06f6170c4dee62990bd3700b1460e1d54fa4f0a0
luismiguel172 <luis.goncalves68@etec.sp.gov.br>
2023-08-12 17:09:29 -0300

    feat: Tela Perfil adm

---
+++ src/pages/dashboard/perfil.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Main } from "../../../layout/Main";
+import { Container } from "../../../layout/Container";
+import { Flex, Text } from "@chakra-ui/react";
+
+function Perfil(){
+    return(
+     <Main>
+       <Container>
+    
+
+
+       </Container>
+    </Main>
+ );       
+}
+
+export default Perfil;
